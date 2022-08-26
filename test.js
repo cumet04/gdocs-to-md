4a85d87db2276fc7cdcce4a3baacffe38915a217
cumet04 <medalhkr@gmail.com>
2022-08-26 09:19:45 +0900

    test

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+import { convert } from './convert.js'
+
 const list = document.getElementById("list");
 const skeleton = (() => {
   const s = document.getElementsByClassName("case")[0];
@@ -9,8 +11,7 @@ const skeleton = (() => {
 const cases = [
   {
     input: `<span id="docs-internal-guid-8122520c-7fff-d37b-062d-c21f3df4757c"><h1 dir="ltr" style="line-height:1.38;margin-top:20pt;margin-bottom:6pt;"><span style="font-size: 20pt; font-family: Arial; font-weight: 400; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">みだし1</span></h1><p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;"><span style="font-size: 11pt; font-family: Arial; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">plain text</span></p><ul style="margin-top:0;margin-bottom:0;padding-inline-start:48px;"><li dir="ltr" style="list-style-type: disc; font-size: 11pt; font-family: Arial; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre;" aria-level="1"><p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;" role="presentation"><span style="font-size: 11pt; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">箇条書き1</span></p></li><ul style="margin-top:0;margin-bottom:0;padding-inline-start:48px;"><li dir="ltr" style="list-style-type: circle; font-size: 11pt; font-family: Arial; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre;" aria-level="2"><p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;" role="presentation"><span style="font-size: 11pt; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">インデント</span></p></li></ul><li dir="ltr" style="list-style-type: disc; font-size: 11pt; font-family: Arial; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre;" aria-level="1"><p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;" role="presentation"><span style="font-size: 11pt; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">か</span></p></li></ul><h3 dir="ltr" style="line-height:1.38;margin-top:16pt;margin-bottom:4pt;"><span style="font-size: 14pt; font-family: Arial; color: rgb(67, 67, 67); font-weight: 400; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">みだし3</span></h3><ul style="margin-top:0;margin-bottom:0;padding-inline-start:48px;"><li dir="ltr" style="list-style-type: disc; font-size: 11pt; font-family: Arial; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre;" aria-level="1"><p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;" role="presentation"><span style="font-size: 11pt; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">hoge</span></p></li><li dir="ltr" style="list-style-type: disc; font-size: 11pt; font-family: Arial; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre;" aria-level="1"><p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;" role="presentation"><span style="font-size: 11pt; font-variant-numeric: normal; font-variant-east-asian: normal; vertical-align: baseline; white-space: pre-wrap;">fuga</span></p></li></ul></span>`,
-    want: `
-# みだし1
+    want: `# みだし1
 plain text
 * 箇条書き1
   * インデント
@@ -22,7 +23,17 @@ plain text
   }
 ];
 
-cases.forEach((case) => {
+cases.forEach((c) => {
   const node = list.appendChild(skeleton.cloneNode(true));
-  // TODO
+  const input = node.getElementsByClassName("input-area")[0];
+  const output = node.getElementsByClassName("output-area")[0];
+
+  input.innerHTML = c.input;
+  const actual = convert(input);
+  output.innerText = actual;
+
+  // output.innerTextで比較すると何故か差が出る。innerTextの方が2文字少ないことは分かっているが、詳細は調べてない。
+  if (actual != c.want) {
+    output.style['borderColor'] = 'red';
+  }
 })
